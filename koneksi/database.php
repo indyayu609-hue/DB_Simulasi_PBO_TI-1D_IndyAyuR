ae8d340e34ab313c5db47c811045735f2fd5b49f
indyayu609-hue.com <indyayu609@gmail>
2026-06-18 14:46:59 +0700

    [Tahap 3] Membuat abstract class Pendaftaran dan koneksi database

--- koneksi/database.php
+++ koneksi/database.php
@@ -3,26 +3,20 @@
 
 class Database {
     private $host = "localhost";
-    private $db_name = "db_simulasi_pb0_ti_1d_indyayur"; // Menyesuaikan nama DB di file SQL Anda
+    private $db_name = "db_simulasi_pb0_ti_1d_indyayur"; // Nama database sesuai file .sql Anda
     private $username = "root";
     private $password = "";
     public $conn;
 
+    // Poin 1: Menangani koneksi database menggunakan PDO
     public function getConnection() {
         $this->conn = null;
-        
         try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
-                $this->username, 
-                $this->password
-            );
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch(PDOException $exception) {
-            echo "Kesalahan Koneksi: " . $exception->getMessage();
+            echo "Koneksi gagal: " . $exception->getMessage();
         }
-        
         return $this->conn;
     }
 }
